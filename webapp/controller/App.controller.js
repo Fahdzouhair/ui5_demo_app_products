459678d4f86632a433756b08ca8edeb53ac85527
fahdzouhair <fahdzouhair47@gmail.com>
2025-08-22 22:05:36 +0100

    2.3-Sort & Group & Filter

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,27 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
     "sap/ui/core/Fragment",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/Filter",
     "ui5/demo/app/model/models",
     "ui5/demo/app/model/formatter"],
-    (Controller, Fragment, models, formatter) => {
+    (Controller, Fragment, Sorter, Filter, models, formatter) => {
         'use strict'
 
 
 
 
         return Controller.extend('ui5.demo.app.controller.App', {
+            
             formatter: formatter,
-
-            onPressProduct: function () {
+            _oDialogP : {},
+            fragmentNames : {
+                createProduct : "ui5.demo.app.view.fragments.CreateProduct",
+                sortProduct   : "ui5.demo.app.view.fragments.SortDialog",
+                groupProduct  : "ui5.demo.app.view.fragments.GroupDialog",
+                filtreProduct : "ui5.demo.app.view.fragments.FilterDialog"
+            },
+           
+            onPressProduct: async function () {
 
                 if ( !this._validateInputData() ) return 
 
@@ -23,7 +33,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
                 oItems.push(oInput);
                 oProduct.refresh()
 
-                this._oCreateProductDialog.close();
+                const oDialog = await this._openDialog(this.fragmentNames.createProduct);
+                oDialog.close();
 
 
             },
@@ -40,26 +51,77 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
             
             onAfterClose: function(){
                 this.getOwnerComponent().setModel(models.createProductModel() , "input");
-                this.getOwnerComponent().setMode(models.validatePeoductModel(), "validate")
+                this.getOwnerComponent().setModel(models.validatePeoductModel(), "validate")
             },
 
-            onPressAddNewProduct: function () {
-                if (!this._oCreateProductDialog) {
-                    Fragment.load({
-                        id: this.getView().getId(),
-                        name: "ui5.demo.app.view.fragments.CreateProduct",
-                        controller: this
-                    }).then(oDialog => {
-                        this._oCreateProductDialog = oDialog
-                        this.getView().addDependent(oDialog)
-                        oDialog.open()
-                    })
-                } else {
-                    this._oCreateProductDialog.open()
-                }
+            onPressAddNewProduct:async function () {
+                const oDialog = await this._openDialog(this.fragmentNames.createProduct)
+                oDialog.open()
             },
-            onPressCancelNewProduct: function () {
-                this._oCreateProductDialog.close()
+            onPressCancelNewProduct: async function () {
+                const oDialog = await this._openDialog(this.fragmentNames.createProduct)
+                oDialog.close();
+               
+            },
+            onSortProduct: async function(){
+                const oDialog = await this._openDialog(this.fragmentNames.sortProduct);
+                oDialog.open()
+            },
+            onGroupProduct : async function(){
+                const oDialog = await this._openDialog(this.fragmentNames.groupProduct);
+                oDialog.open()
+            },
+            onFilterProduct : async function(){
+                const oDialog = await this._openDialog(this.fragmentNames.filtreProduct);
+                oDialog.open()
+            },
+            onConfirmSort: function(oEvent){
+                const oItems = oEvent.getParameter("sortItem");
+                const bDes = oEvent.getParameter("sortDescending");
+
+                this.getView()
+                    .byId("idProductList")
+                    .getBinding("items")
+                    .sort(
+                        oItems ? [new Sorter(oItems.getKey() , bDes) ] : []
+                    );
+                console.log(
+                    this.getView()
+                    .byId("idProductList")
+                    .getBinding("items")
+                    
+                )
+            },
+            onConfirmGroup : function(oEvent){
+                const oGroupItems = oEvent.getParameter("groupItem");
+                const bDes = oEvent.getParameter("groupDescending");
+                this.getView()
+                    .byId("idProductList")
+                    .getBinding("items")
+                    .sort(
+                        oGroupItems ? [new Sorter(oGroupItems.getKey() , bDes , true) ] : []
+                    );
+                
+            },
+            onConfirmFilter: function(oEvent){
+                const aFilterItems = oEvent.getParameter("filterItems");
+                const aFilterString = oEvent.getParameter("filterString");
+                console.log(aFilterString)
+                
+                const aFilter = [];
+
+                aFilterItems.forEach(item =>{
+                    const [sPath , oOerator , sValue1 , sValue2] = item.getKey().split('__');
+                    aFilter.push(new Filter(sPath , oOerator , sValue1 , sValue2))
+                })
+                this.getView()
+                    .byId("idProductList")
+                    .getBinding("items")
+                    .filter(aFilter)
+
+                this.getView().byId("idFilterInfoToolbar").setVisible(aFilter.length > 0 ? true : false )
+                this.getView().byId("idFilterText").setText(aFilterString)
+
             },
             _validateInputData: function () {
                 const oInput = this.getView().getModel("input").getData();
@@ -76,6 +138,22 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
                     return oValidateData[key] === true
                 });
 
+
+            },
+            _openDialog :async function(fragmentView,sDialogId){
+
+                if(!!!this._oDialogP[fragmentView]){
+                    this._oDialogP[fragmentView] = Fragment.load({
+                        id : this.getView().createId(sDialogId),
+                        name : fragmentView,
+                        controller: this
+                    }).then(oDlg => {
+                        this.getView().addDependent(oDlg);
+                        return oDlg;
+                    })
+                }
+                
+                return await this._oDialogP[fragmentView];
 
             }
         })
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,15 +1,41 @@
-sap.ui.define(["sap/ui/core/library",], function (coreLibrary) {
-        'use strict'
-        const { ValueState } = coreLibrary;
-        return {
-            
-            formatAvailabilityText(sValue) {
-                const oBundle = this.getOwnerComponent().getModel('i18n').getResourceBundle()
-                return sValue && new Date(sValue) <= new Date() ? oBundle.getText('unavailable') : oBundle.getText('available')
-            },
-           
-            formatAvailabilityState(sValue) {
-                return sValue && new Date(sValue) <= new Date() ? ValueState.Error : ValueState.Success
-            },
+sap.ui.define(["sap/ui/core/library",
+               "sap/m/GroupHeaderListItem"], function (coreLibrary,GroupHeaderListItem) {
+    'use strict'
+    const { ValueState } = coreLibrary;
+    return {
+
+        formatAvailabilityText(sValue) {
+            const oBundle = this.getOwnerComponent().getModel('i18n').getResourceBundle()
+            return sValue && new Date(sValue) <= new Date() ? oBundle.getText('unavailable') : oBundle.getText('available')
+        },
+
+        formatAvailabilityState(sValue) {
+            return sValue && new Date(sValue) <= new Date() ? ValueState.Error : ValueState.Success
+        },
+        formatCategory(sValue) {
+            switch (sValue) {
+                case "0":
+                    return 'Food'
+                case "1":
+                    return 'Beveages'
+                case "2":
+                    return 'Electronics'
+                default:
+                    return 'Unknown'
+
+            }
+        },
+        formatGroupHeader(oGroup) {
+            switch (oGroup.key) {
+                case '0':
+                    return new GroupHeaderListItem({ title: 'Food' })
+                case '1':
+                    return new GroupHeaderListItem({ title: 'Beverages' })
+                case '2':
+                    return new GroupHeaderListItem({ title: 'Electronics' })
+                default:
+                    return new GroupHeaderListItem({ title: 'Unknown' })
+            }
         }
-    })
+    }
+})
